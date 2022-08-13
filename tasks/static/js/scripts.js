7f9f868c6768fd636a198d1103324568351d2852
sidivianna <sidineirochavianna19@gmail.com>
2022-08-13 15:14:45 -0300

    Paginação do projeto

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,17 @@
-console.log('Funcionou!!! ');
+$( document ).ready(function() {
+
+    var deleteBnt = $('delete-btn');
+
+    $(deleteBtn).on('click', function(e) {
+        
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(refult) {
+            window.location.href = delLink;
+        }
+    });
+
+});
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm # importar o fomrulário
 from django.contrib import messages
@@ -6,7 +7,13 @@ from django.contrib import messages
 from .models import Task
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at') #ordenar do mais novo para o mais antigo.
+    tasks_list = Task.objects.all().order_by('-created_at') #ordenar do mais novo para o mais antigo.
+    paginator = Paginator(tasks_list, 3)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html', {'tasks':tasks})
 
 def taskView(request, id):
